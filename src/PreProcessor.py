5a152340b56c1fa23f4463373249133418eada9c
FelipeBuzato <felipebuzatofc@gmail.com>
2026-05-23 17:46:47 -0300

    added preprocess config

--- src/PreProcessor.py
+++ src/PreProcessor.py
@@ -1,17 +1,54 @@
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
+import json
 
 class PreProcessor:
-    def __init__(self, numerical_cols, categorical_cols):
-        self.numerical_cols = numerical_cols
-        self.categorical_cols = categorical_cols
+    def __init__(self, df):
+        with open('config_preprocess.json', 'r') as file:
+            self.config = json.load(file)
+        column_groups = self.split_cols(df)
 
+        self.numerical_cols = column_groups[0]
+        self.categorical_cols = column_groups[1]
+        self.ordinal_1_cols = column_groups[2]
+        self.ordinal_2_cols = column_groups[3]
+        self.na_as_none_cols = column_groups[4]
+        self.num_as_zero_cols = column_groups[5]
 
+        self.ordinal_1_categories = list(self.config["ordinal_1"].values())
+        self.ordinal_2_categories = list(self.config["ordinal_2"].values())
+        
+
+    def split_cols(self, df):
+        numeric_cols = set(df.select_dtypes(include="number").columns)
+        categorical_cols = set(df.select_dtypes(include=["object", "category"]).columns)
+
+        ordinal_1_cols = set(self.config["ordinal_1"].keys())
+        ordinal_2_cols = set(self.config["ordinal_2"].keys())
+        na_as_none_cols = set(self.config["na_as_none"])
+        num_as_zero_cols = set(self.config["num_as_zero"])
+        special_cols = (ordinal_1_cols | ordinal_2_cols | na_as_none_cols | num_as_zero_cols)
+        
+        numerical_cols = list(numeric_cols - special_cols)
+        categorical_cols = list(categorical_cols - special_cols)
+        ordinal_1_cols = list(ordinal_1_cols)
+        ordinal_2_cols = list(ordinal_2_cols)
+        na_as_none_cols = list(na_as_none_cols)
+        num_as_zero_cols = list(num_as_zero_cols)
+
+        return numerical_cols, categorical_cols, ordinal_1_cols, ordinal_2_cols, na_as_none_cols, num_as_zero_cols
+
+    # Change handle unknown strategy!!!
     def build(self):
-        numeric_pipeline = Pipeline([
+        numerical_pipeline = Pipeline([
             ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", StandardScaler())
+        ])
+
+        num_as_zero_pipeline = Pipeline([
+            ("imputer", SimpleImputer(strategy="constant", fill_value=0)),
             ("scaler", StandardScaler())
         ])
 
@@ -20,9 +57,30 @@ class PreProcessor:
             ("onehot", OneHotEncoder(handle_unknown="ignore"))
         ])
 
+        na_none_pipeline = Pipeline([
+            ("imputer", SimpleImputer(strategy="constant", fill_value="None")),
+            ("onehot", OneHotEncoder(handle_unknown="ignore"))
+        ])
+
+        ordinal_1_pipeline = Pipeline([
+            ("inputer", SimpleImputer(strategy='constant', fill_value="None")),
+            ("encoder", OrdinalEncoder(categories=self.ordinal_1_categories, handle_unknown="use_encoded_value", unknown_value=-1)),
+            ("scaler", StandardScaler())
+        ])
+
+        ordinal_2_pipeline = Pipeline([
+            ("inputer", SimpleImputer(strategy='most_frequent')),
+            ("encoder", OrdinalEncoder(categories=self.ordinal_2_categories, handle_unknown="use_encoded_value", unknown_value=-1)),
+            ("scaler", StandardScaler())
+        ])
+
         column_transformer = ColumnTransformer([
-            ("num", numeric_pipeline, self.numerical_cols),
-            ("cat", categorical_pipeline, self.categorical_cols)
+            ("num", numerical_pipeline, self.numerical_cols),
+            ("cat", categorical_pipeline, self.categorical_cols),
+            ("ord_1", ordinal_1_pipeline, self.ordinal_1_cols),
+            ("ord_2", ordinal_2_pipeline, self.ordinal_2_cols),
+            ("non_exist", na_none_pipeline, self.na_as_none_cols),
+            ("num_as_zero", num_as_zero_pipeline, self.num_as_zero_cols)
         ])
 
         return column_transformer
